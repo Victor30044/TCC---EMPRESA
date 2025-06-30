6ec0183db0f6d8637df44d080793d7e11d40b661
Arthur de Almeida Cecconi <arthurcecconi08@gmail.com>
2025-06-30 13:55:29 -0300

    atualizacao do bd

--- database.js
+++ database.js
@@ -1,16 +1,6 @@
 const Database = require('better-sqlite3');
 const db = new Database('./pizzaria.db');
 
-
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS Produtos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    descricao TEXT,
-    preco REAL NOT NULL
-  )`);
-});
-
 db.close(() => {
   console.log('Tabela Produtos criada com sucesso!');
 });
